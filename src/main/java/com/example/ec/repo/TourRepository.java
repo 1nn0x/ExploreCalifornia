db9e11edc27e5c791969bbb98fb4e336e9b815fb
Gwedoh <163425j35U5777!>
2017-01-05 18:34:47 -0600

    Built out basic REST APIs

--- src/main/java/com/example/ec/repo/TourRepository.java
+++ src/main/java/com/example/ec/repo/TourRepository.java
@@ -5,12 +5,43 @@ import com.example.ec.domain.Region;
 import com.example.ec.domain.Tour;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+import java.awt.print.Pageable;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 
-public interface TourRepository extends CrudRepository<Tour,Integer> {
-    List<Tour> findByTourPackageCode(@Param("code")String code);
+public interface TourRepository extends CrudRepository<Tour, Integer> {
+    List<Tour> findByTourPackageCode(@Param("code") String code);
+
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> save(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour tour);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Iterable<? extends Tour> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
+
+
 }
